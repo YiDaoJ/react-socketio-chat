4e05a774dca39836e717f0a9e8141fa1c6d54045
Xiao <chuxjiang@deloitte.de>
2023-04-07 20:58:04 +0200

    add client side based on vite

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,37 @@
-import React, { useState } from 'react';
-import './App.css';
-import io from 'socket.io-client'
-import Chat from './components/Chat';
-import { RoomEntry } from './components/RoomEntry';
+import React, { useState } from "react";
+import "./App.css";
+import viteLogo from "/vite.svg";
+import io from "socket.io-client";
+import Chat from "./components/Chat";
+import { RoomEntry } from "./components/RoomEntry";
 
-const socket = io("http://localhost:3005")
+const socket = io("http://localhost:3005");
 
 const App: React.FC = () => {
-
-  const [username, setUsername] = useState("")
-  const [room, setRoom] = useState("")
-  const [showChat, setShowChat] = useState(false)
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
 
   const joinRoom = () => {
-    if(username && room) {
-      socket.emit("join_room", {room})
-      setShowChat(true)
+    if (username && room) {
+      socket.emit("join_room", { room });
+      setShowChat(true);
     }
-  }
+  };
 
   return (
     <div className="App">
-      {
-        !showChat ? (
-          <RoomEntry joinRoom={joinRoom} setRoom={setRoom} setUsername={setUsername} />
-        ) : (
-          <Chat socket={socket} username={username} room={room} />
-        )
-      }
+      {!showChat ? (
+        <RoomEntry
+          joinRoom={joinRoom}
+          setRoom={setRoom}
+          setUsername={setUsername}
+        />
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
     </div>
   );
-}
+};
 
 export default App;
